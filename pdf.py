20e68bb127c50f18101a8c63416b40e734711726
dannywoot <128287796+dannywoot@users.noreply.github.com>
2023-04-22 14:37:03 +0200

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,14 +1,13 @@
 import pdfplumber
 import openai
+import concurrent.futures
 import os
 import tkinter as tk
 from tkinter import filedialog
+from functools import lru_cache
 
 # Set your OpenAI API key
 openai.api_key = "YOUR_API"
-
-# Function to extract text from a PDF file
-import concurrent.futures
 
 def extract_text_from_page(page):
     return page.extract_text()
@@ -21,38 +20,35 @@ def extract_text_from_pdf(pdf_path):
         text = ' '.join(page_texts)
     return text
 
-# Function to send a message to the GPT API
+@lru_cache(maxsize=100)
 def send_message_to_gpt(prompt):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1500,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    message = response.choices[0].text.strip()
-    return message
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1500,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        message = response.choices[0].text.strip()
+        return message
+    except Exception as e:
+        print(f"Error: {e}")
+        return "An error occurred while processing the request."
 
-# Function to divide the text into smaller sections
 def divide_text(text, max_tokens):
     sections = []
-    tokens = text.split()
-    current_section = []
-
-    for token in tokens:
-        if len(" ".join(current_section)) + len(token) + 1 <= max_tokens:
-            current_section.append(token)
-        else:
-            sections.append(" ".join(current_section))
-            current_section = [token]
-
-    if current_section:
-        sections.append(" ".join(current_section))
-
+    start = 0
+    end = max_tokens
+    while start < len(text):
+        if end < len(text) and text[end] != " ":
+            end = text.rfind(" ", start, end)
+        sections.append(text[start:end].strip())
+        start = end + 1
+        end = start + max_tokens
     return sections
 
-# Function to get information about a PDF
 def analyze_pdf(pdf_text, question):
     max_tokens = 4096 - 150
     sections = divide_text(pdf_text, max_tokens)
@@ -65,9 +61,7 @@ def analyze_pdf(pdf_text, question):
 
     return "\n".join(responses)
 
-# Function to select the PDF folder and start the conversation
 def main():
-    # Create a simple directory dialog for selecting the PDF folder
     root = tk.Tk()
     root.withdraw()
     folder_path = filedialog.askdirectory()
@@ -83,20 +77,19 @@ def main():
         if user_input.lower() == 'quit':
             break
 
-        # Open a file dialog for selecting a PDF
         selected_pdf = filedialog.askopenfilename(initialdir=folder_path, title="Select a PDF", filetypes=(("PDF files", "*.pdf"), ("All files", "*.*")))
 
         if not selected_pdf:
             print("GPT: No PDF was selected.")
             continue
 
-        # Extract and store the text for each PDF only once
         if selected_pdf not in pdf_texts:
             pdf_texts[selected_pdf] = extract_text_from_pdf(selected_pdf)
-        
+
         pdf_text = pdf_texts[selected_pdf]
         analysis = analyze_pdf(pdf_text, user_input)
         print(f"GPT:\n{analysis}")
 
 if __name__ == "__main__":
     main()
+    
